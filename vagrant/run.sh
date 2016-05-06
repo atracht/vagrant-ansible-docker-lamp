81545e903c8bbfb63245b33cf59bdc01006b5cae
Adam Tracht <adam.tracht@gmail.com>
2016-05-05 21:45:55 -0400

    Moved provisioner to shell script

---
+++ vagrant/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Allow script to be run without input
+sudo DEBIAN_FRONTEND=noninteractive
+
+# Update and intall repositories
+sudo apt-get update
+sudo apt-get -y install software-properties-common
+
+# Install Ansible
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get -y install ansible
